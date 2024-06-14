emqx/rocketmq-client-erl

commit d21a85c4e21d7fcb6506ce7e7d7e0b58a341498c

fix: return error if sending data to socket fails

src/rocketmq_producer.erl
@@ -175,8 +175,19 @@ connected({call, From}, {send, MsgAndProps}, State = #state{sock = Sock,
                                                         producer_opts = ProducerOpts,
                                                         sock_mod = SendSockMod
                                                         }) ->
-    send(Sock, ProducerGroup, get_namespace(ProducerOpts), Topic, Opaque, QueueId, MsgAndProps, get_acl_info(ProducerOpts), SendSockMod),
-    {keep_state, next_opaque_id(State#state{requests = maps:put(Opaque, From, Reqs)})};
+    SendRes =
+        send(Sock,
+             ProducerGroup,
+             get_namespace(ProducerOpts),
+             Topic,
+             Opaque,
+             QueueId,
+             MsgAndProps,
+             get_acl_info(ProducerOpts),
+             SendSockMod),
+    {keep_state,
+     next_opaque_id(State#state{requests = maps:put(Opaque, From, Reqs)}),
+     [{reply, From, SendRes}]};
 
 connected({call, From}, {batch_send, Messages}, State = #state{sock = Sock,
                                                         topic = Topic,
@@ -187,8 +198,19 @@ connected({call, From}, {batch_send, Messages}, State = #state{sock = Sock,
                                                         producer_opts = ProducerOpts,
                                                         sock_mod = SendSockMod
                                                         }) ->
-    batch_send(Sock, ProducerGroup, get_namespace(ProducerOpts), Topic, Opaque, QueueId, Messages, get_acl_info(ProducerOpts), SendSockMod),
-    {keep_state, next_opaque_id(State#state{requests = maps:put(Opaque, From, Reqs)})};
+    SendRes =
+        batch_send(Sock,
+                   ProducerGroup,
+                   get_namespace(ProducerOpts),
+                   Topic,
+                   Opaque,
+                   QueueId,
+                   Messages,
+                   get_acl_info(ProducerOpts),
+                   SendSockMod),
+    {keep_state,
+     next_opaque_id(State#state{requests = maps:put(Opaque, From, Reqs)}),
+     [{reply, From, SendRes}]};
 
 connected(cast, {send, MsgAndProps}, State = #state{sock = Sock,
                                                 topic = Topic,
@@ -200,16 +222,37 @@ connected(cast, {send, MsgAndProps}, State = #state{sock = Sock,
                                                 requests = Requests,
                                                 sock_mod = SendSockMod
                                                 }) ->
-    BatchLen =
+    {BatchLen, SendRes} =
         case BatchSize =< 1 of
             true ->
-                _ = send(Sock, ProducerGroup, get_namespace(ProducerOpts), Topic, Opaque, QueueId, MsgAndProps, get_acl_info(ProducerOpts), SendSockMod),
-                1;
+                SendRes1 = send(Sock,
+                                ProducerGroup,
+                                get_namespace(ProducerOpts),
+                                Topic,
+                                Opaque,
+                                QueueId,
+                                MsgAndProps,
+                                get_acl_info(ProducerOpts),
+                                SendSockMod),
+                {1, SendRes1};
             false ->
                 MsgPropsList = [MsgAndProps | collect_send_calls(BatchSize)],
-                _ = batch_send(Sock, ProducerGroup, get_namespace(ProducerOpts), Topic, Opaque, QueueId, MsgPropsList, get_acl_info(ProducerOpts), SendSockMod),
-                erlang:length(MsgPropsList)
+                SendRes2 = batch_send(Sock,
+                                      ProducerGroup,
+                                      get_namespace(ProducerOpts),
+                                      Topic,
+                                      Opaque,
+                                      QueueId,
+                                      MsgPropsList,
+                                      get_acl_info(ProducerOpts),
+                                      SendSockMod),
+                {erlang:length(MsgPropsList), SendRes2}
         end,
+    case SendRes of
+        ok -> ok;
+        Error ->
+            log(error, "Async send returned error: ~p", [Error])
+    end,
     NRequests = maps:put(Opaque, {batch_len, BatchLen}, Requests),
     NState = next_opaque_id(State),
     {keep_state, NState#state{requests = NRequests}};

commit b5f6334e85fbf6778ec46b43ae430f173fdc7d22

fix: broken send resonse handling
This commit fixes an error introduced in commit
d21a85c4e21d7fcb6506ce7e7d7e0b58a341498c . If sending data to socket
works without errors we should wait until we get an acknowledgment from
the server until we respond to the send caller.

This has been tested by running the EMQX RocketMQ bridge test suite
which detected this error.

src/rocketmq_producer.erl
@@ -185,9 +185,7 @@ connected({call, From}, {send, MsgAndProps}, State = #state{sock = Sock,
              MsgAndProps,
              get_acl_info(ProducerOpts),
              SendSockMod),
-    {keep_state,
-     next_opaque_id(State#state{requests = maps:put(Opaque, From, Reqs)}),
-     [{reply, From, SendRes}]};
+    handle_socket_send_result(State, SendRes, Opaque, From, Reqs);
 
 connected({call, From}, {batch_send, Messages}, State = #state{sock = Sock,
                                                         topic = Topic,
@@ -208,9 +206,7 @@ connected({call, From}, {batch_send, Messages}, State = #state{sock = Sock,
                    Messages,
                    get_acl_info(ProducerOpts),
                    SendSockMod),
-    {keep_state,
-     next_opaque_id(State#state{requests = maps:put(Opaque, From, Reqs)}),
-     [{reply, From, SendRes}]};
+    handle_socket_send_result(State, SendRes, Opaque, From, Reqs);
 
 connected(cast, {send, MsgAndProps}, State = #state{sock = Sock,
                                                 topic = Topic,
@@ -268,6 +264,16 @@ connected(_EventType, ping, State = #state{sock = Sock,
 
 connected(_EventType, EventContent, State) ->
     handle_response(EventContent, State).
+
+handle_socket_send_result(State, {error, _} = SendRes, Opaque, From, Reqs) ->
+    {keep_state,
+     next_opaque_id(State#state{requests = maps:put(Opaque, From, Reqs)}),
+     [{reply, From, SendRes}]};
+handle_socket_send_result(State, ok = _SendRes, Opaque, From, Reqs) ->
+     %% Reply will be sent by handle_response/2 if the request do not time
+     %% out
+     {keep_state,
+     next_opaque_id(State#state{requests = maps:put(Opaque, From, Reqs)})}.
 
 code_change(_Vsn, State, Data, _Extra) ->
     {ok, State, Data}.